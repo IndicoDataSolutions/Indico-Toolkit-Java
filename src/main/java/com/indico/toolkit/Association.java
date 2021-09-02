2e86b046695d5b77bf206e8f9abbb285657cc271
Scott <scott.levin@indico.io>
2021-09-02 13:20:55 -0400

    testing numberPredictions works

--- src/main/java/com/indico/toolkit/Association.java
+++ src/main/java/com/indico/toolkit/Association.java
@@ -8,23 +8,23 @@ import java.util.Comparator;
 import java.util.Collections;
 
 public class Association{
-    public ArrayList<Prediction> predictions;
-    public ArrayList<String> lineItemFields;
-    private ArrayList<Prediction> mappedPositions = new ArrayList<Prediction>();
-    private ArrayList<Prediction> unmappedPositions = new ArrayList<Prediction>();
+    public List<Prediction> predictions;
+    public List<String> lineItemFields;
+    private List<Prediction> mappedPositions = new ArrayList<Prediction>();
+    private List<Prediction> unmappedPositions = new ArrayList<Prediction>();
 
-    public Association(ArrayList<Prediction> predictions, ArrayList<String> lineItemFields){
+    public Association(List<Prediction> predictions, List<String> lineItemFields){
         this.predictions = predictions;
         this.lineItemFields = lineItemFields;
     }
 
-    public void getBoundingBoxes(ArrayList<Prediction> tokens){
-        ArrayList<Prediction> preds = this.removeUnneededPredictions(this.predictions);
+    public void getBoundingBoxes(List<Prediction> tokens){
+        List<Prediction> preds = this.removeUnneededPredictions(this.predictions);
 
     }
 
-    private ArrayList<Prediction> removeUnneededPredictions(ArrayList<Prediction> predictions){
-        ArrayList<Prediction> neededPreds = new ArrayList<Prediction>();
+    private List<Prediction> removeUnneededPredictions(List<Prediction> predictions){
+        List<Prediction> neededPreds = new ArrayList<>();
         for(Prediction pred: predictions){
             if(this.lineItemFields.contains(pred.label)){
                 neededPreds.add(pred);
--- src/test/java/com/indico/toolkit/TestAssociation.java
+++ src/test/java/com/indico/toolkit/TestAssociation.java
@@ -1,6 +1,5 @@
 package com.indico.toolkit;
 
-
 import com.indico.toolkit.Association;
 import com.indico.toolkit.Prediction;
 
@@ -10,22 +9,42 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.google.common.io.Resources;
+import java.lang.reflect.Type;
+import java.lang.Object;
+import com.google.gson.reflect.TypeToken;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestAssociation{
-    Prediction[] predictions;
+    List<Prediction> predictions = null;
+    List<Object> tokens = null;
+    ArrayList<String> line_fields = new ArrayList<>();
 
+    @BeforeAll
+    public void setup() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        Gson gson = new Gson();
+        Type predType = new TypeToken<ArrayList<Prediction>>(){}.getType();
+        Type tokenType = new TypeToken<ArrayList<Object>>(){}.getType();
+        String predsLoc = classLoader.getResource("preds.json").getFile().toString();
+        Reader predsReader = Files.newBufferedReader(Paths.get(predsLoc));
+        predictions = gson.fromJson(predsReader, predType);
+        String tokensLoc = classLoader.getResource("tokens.json").getFile().toString();
+        Reader tokenReader = Files.newBufferedReader(Paths.get(tokensLoc));
+        tokens = gson.fromJson(predsReader, tokenType);
+        line_fields.add("A");
+        line_fields.add("B");
+        line_fields.add("C");
+    }
     @Test
     void testNumberPredictions() throws IOException {
-        Assertions.assertEquals(1, 1);
-        ClassLoader classLoader = getClass().getClassLoader();
-        String predsLoc = classLoader.getResource("preds.json").getFile().toString();
-        Reader reader = Files.newBufferedReader(Paths.get(predsLoc));
-        Gson gson = new Gson();
-        Prediction[] preds = gson.fromJson(reader, Prediction[].class);
-        Assertions.assertEquals(preds, "hi!");
+        Association associate = new Association(predictions, line_fields);
+        Assertions.assertEquals(associate.numberPredictions(), 5);
     }
 }
